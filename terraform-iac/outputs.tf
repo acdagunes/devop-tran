221285f98de9a7c383a8b5238c0b87333aee78e5
DevOps Learner <devops.learner@mylocal.com>
2025-11-24 19:46:21 +0400

    feat: Added Nginx Load Balancer IP output to outputs.tf

--- terraform-iac/outputs.tf
+++ terraform-iac/outputs.tf
@@ -1,7 +1,11 @@
-# outputs.tf
+# terraform-iac/outputs.tf (განახლებული ვერსია)
 
-# terraform-iac/outputs.tf
+output "nginx_load_balancer_ip" {
+  description = "The external IP address of the Nginx Load Balancer service."
+  value       = kubernetes_service.nginx_loadbalancer.status[0].load_balancer[0].ingress[0].ip
+}
 
+# შეგიძლია ძველი kube_config-ის Output-იც დატოვო, თუმცა ის Sensitive-ია და ლოგებში არ გამოჩნდება.
 output "kube_config" {
   value     = digitalocean_kubernetes_cluster.k8s_cluster.kube_config.0.raw_config
   sensitive = true
