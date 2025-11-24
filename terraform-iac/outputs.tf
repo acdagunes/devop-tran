4e1138acc4b74f699d8f7d7e2ff17f047e55f082
DevOps Learner <devops.learner@mylocal.com>
2025-11-24 23:22:12 +0400

    feat: Updated outputs.tf to reflect AWS EC2 public IP and commented out old K8s output.

--- terraform-iac/outputs.tf
+++ terraform-iac/outputs.tf
@@ -1,12 +1,21 @@
 # terraform-iac/outputs.tf (განახლებული ვერსია)
-
-output "nginx_load_balancer_ip" {
-  description = "The external IP address of the Nginx Load Balancer service."
-  value       = kubernetes_service.nginx_loadbalancer.status[0].load_balancer[0].ingress[0].ip
+output "aws_instance_public_ip" {
+  description = "Public IP of the created AWS EC2 instance"
+  value       = aws_instance.devops_droplet.public_ip
 }
+
+
+
+
+
+
+#output "nginx_load_balancer_ip" {
+#  description = "The external IP address of the Nginx Load Balancer service."
+#  value       = kubernetes_service.nginx_loadbalancer.status[0].load_balancer[0].ingress[0].ip
+#}
 
 # შეგიძლია ძველი kube_config-ის Output-იც დატოვო, თუმცა ის Sensitive-ია და ლოგებში არ გამოჩნდება.
-output "kube_config" {
-  value     = digitalocean_kubernetes_cluster.k8s_cluster.kube_config.0.raw_config
-  sensitive = true
-}
+#output "kube_config" {
+#  value     = digitalocean_kubernetes_cluster.k8s_cluster.kube_config.0.raw_config
+#  sensitive = true
+#}
